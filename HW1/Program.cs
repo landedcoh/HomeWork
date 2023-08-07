86828185d006ad52b068b4c70834ff90aeb6b36d
landed <coh2@yandex.ru>
2023-08-08 01:19:53 +0300

    HW1

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите число 1 ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2 ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 < number1)
+{
+    Console.Write("Большее число 1, а меньшее число 2");
+}
+else 
+{
+    Console.Write("Большее число 2, а меньшее число 1");
+}
