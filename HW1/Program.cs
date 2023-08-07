294ead5be7ade1cf071ba12cf7cab3eb446feaef
landed <coh2@yandex.ru>
2023-08-08 01:49:10 +0300

    HW1_1

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,15 +1,21 @@
 ﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.Write("Введите число 1 ");
+Console.Write("Введите число a ");
 int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число 2 ");
+Console.Write("Введите число b ");
 int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+int min = number2;
 
 if (number2 < number1)
 {
-    Console.Write("Большее число 1, а меньшее число 2");
+    max = number1;
+    min = number2;
+    Console.WriteLine("Большее число a = " + max + " меньшее число b = " + min);
 }
 else 
 {
-    Console.Write("Большее число 2, а меньшее число 1");
+    max = number2;
+    min = number1;
+    Console.Write("Большее число b = " + max + " меньшее число a = " + min);
 }
