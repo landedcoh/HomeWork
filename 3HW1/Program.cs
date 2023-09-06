b9f31cd86c5ee0f48ebae06df2bc15dacdff67bc
landed <coh2@yandex.ru>
2023-09-06 21:28:02 +0300

    3

---
+++ 3HW1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number [1] == number[3])
+    {
+        Console.WriteLine(number + " Палиндром");
+    }
+
+    else
+    {
+        Console.WriteLine(number + " Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine ("Вы ввели НЕ 5-и значное число");
+}
