18dd30468312bab7d3d7eef682cf6a04947c9fdb
landed <coh2@yandex.ru>
2023-09-08 01:10:21 +0300

    4HW2

---
+++ 4HW2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Print(string text)
+{
+Console.WriteLine(text);
+int number = Convert.ToInt32(Console.ReadLine());
+return number;
+}
+int Sum(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+int number = Print("Введите число ");
+Console.WriteLine(Sum(number));
+
+
+
