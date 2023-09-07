6d4569569d191ad7e161ceb0de461303a1165679
landed <coh2@yandex.ru>
2023-09-08 02:38:46 +0300

    4HW3

---
+++ 4HW3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index  = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(1,100);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int position = 0;
+    while (position < count)
+    {
+
+        position++;
+    }
+}
+
+int[]array = new int[8];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+string arrayOutput = String.Join(", ", array);
+Console.WriteLine("[" + arrayOutput + "]");
+
